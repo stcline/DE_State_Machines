519e1db75c93ba1c321bdcdfbaa7078a1dd8dd1c
Steve Cline <steve.cline@weldre4.org>
2025-04-03 19:49:07 -0700

    Add scripts

--- Scripts/LED_test.py
+++ Scripts/LED_test.py
@@ -1,14 +1,23 @@
 import RPi.GPIO as GPIO    # Import Raspberry Pi GPIO library
 from time import sleep     # Import the sleep function from the time module
 
-LED_PIN = 17  # Define the pin number for the LED
+GREEN = 24  # GPIO pin for green LED
+YELLOW = 25    # GPIO pin for yellow LED
+RED = 5  # GPIO pin for red LED
 
 GPIO.setwarnings(False)    # Ignore warning for now
 GPIO.setmode(GPIO.BCM)   # Use physical pin numbering
-GPIO.setup(LED_PIN, GPIO.OUT, initial=GPIO.LOW)   # Set pin 8 to be an output pin and set initial value to low (off)
+GPIO.setup(GREEN, GPIO.OUT, initial=GPIO.LOW)   # Set pin 8 to be an output pin and set initial value to low (off)
+GPIO.setup(YELLOW, GPIO.OUT, initial=GPIO.LOW)   # Set pin 8 to be an output pin and set initial value to low (off)
+GPIO.setup(RED, GPIO.OUT, initial=GPIO.LOW)   # Set pin 8 to be an output pin and set initial value to low (off)
 
 while True: # Run forever
-    GPIO.output(LED_PIN, GPIO.HIGH) # Turn on
+    GPIO.output(GREEN, GPIO.HIGH) # Turn on
     sleep(1)                  # Sleep for 1 second
-    GPIO.output(LED_PIN, GPIO.LOW)  # Turn off
+    GPIO.output(GREEN, GPIO.LOW)  # Turn off
+    GPIO.output(YELLOW, GPIO.HIGH) # Turn on green LED
     sleep(1)                  # Sleep for 1 second
+    GPIO.output(YELLOW, GPIO.LOW)  # Turn off
+    GPIO.output(RED, GPIO.HIGH) # Turn on green LED
+    sleep(1)                  # Sleep for 1 second
+    GPIO.output(RED, GPIO.LOW)  # Turn off
