f66a9d91e68f858f9fe54e6d1c038343f40a355c
Steve Cline <steve.cline@weldre4.org>
2025-04-03 18:25:54 -0700

    Update check_pot.py

---
+++ Scripts/check_pot.py
@@ -0,0 +1,15 @@
+# Import necessary libraries
+import time
+import RPi.GPIO as GPIO
+
+# Set up GPIO pins
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+# Set up pins for the potentiometer
+POT_PIN = 23
+GPIO.setup(POT_PIN, GPIO.IN)
+
+# Function to check the potentiometer
+def check_pot():
+    return GPIO.input(POT_PIN)
